24f1603117efada224b5e9b43d77086e6af8e31f
h <h_user@hserver.home>
2026-05-10 15:43:17 +0000

    [lbs] prep frontend for test deployment

--- rest_api/src/core/mysql_conn_pool.cpp
+++
@@ -1,8 +0,0 @@
-#include <mysql_conn_pool.hpp>
-
-unsigned int GetThreadCount(unsigned int divBy)
-{
-     unsigned int nbThread = std::thread::hardware_concurrency();
-     unsigned int result = nbThread / divBy;
-     return result;
-}
